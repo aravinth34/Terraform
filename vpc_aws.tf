7b8d9a3026e8b05363fc377f8e82e01ccfc10f8b
Aravinth <112575212+aravinth34@users.noreply.github.com>
2023-11-28 19:48:54 +0400

    Update vpc_aws.tf

--- vpc_aws.tf
+++ vpc_aws.tf
@@ -5,5 +5,51 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "vpc_terraform"
   }
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "PublicSubnet"
+ default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["ap-south-1a", "ap-south-1b"]
+}
+
+resource "aws_subnet" "public_subnets" {
+ count             = length(var.public_subnet_cidrs)
+ vpc_id            = aws_vpc.main.id
+ cidr_block        = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ tags = {
+   Name = "Subnet_Pub ${count.index + 1}"
+ }
+}
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+ tags = {
+   Name = "IGW_Terra"
+ }
+}
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ tags = {
+   Name = "Route_Pub"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.second_rt.id
+}
+
+}
