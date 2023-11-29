6d21fcaaebb48f50050af00ef1a3be1332e8d960
Aravinth <112575212+aravinth34@users.noreply.github.com>
2023-11-29 10:42:09 +0400

    Update vpc_aws.tf

--- vpc_aws.tf
+++ vpc_aws.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "default"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
