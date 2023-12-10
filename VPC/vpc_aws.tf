66625defd14e40eb706b439cc2518f8ac008a42c
Aravinth <112575212+maravinth10@users.noreply.github.com>
2023-12-10 21:21:32 +0400

    Update vpc_aws.tf

--- VPC/vpc_aws.tf
+++ VPC/vpc_aws.tf
@@ -102,6 +102,15 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["0.0.0.0/0"]
   //  ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
+//Inbound rule
+  ingress {
+    description      = "Jenkins"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  //  ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
 //Outbound rule
   egress { 
     from_port        = 0
