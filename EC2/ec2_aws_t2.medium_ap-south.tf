c5ca2f87bd1ce8fd92d1cd80a0c7139e8ae751d8
Aravinth <112575212+maravinth10@users.noreply.github.com>
2023-12-16 11:47:14 +0400

    Update ec2_aws_t2.medium_ap-south.tf

--- EC2/ec2_aws_t2.medium_ap-south.tf
+++ EC2/ec2_aws_t2.medium_ap-south.tf
@@ -1,1 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "default"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04708942c263d8190" //Amazon linux ami-02a2af70a66af6dfb
+  instance_type = "t2.medium"
+  key_name      = "mumbai_linux_pem"
+  vpc_security_group_ids = ["sg-092ff5b85daa63af0"]
+  subnet_id     = "subnet-0b3a3a3a2f271cb3b"
+  count         = 1
+  
+  tags = {
+    Name = "nginx_test"
+  }
+}
